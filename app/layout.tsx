9eb0167564caf41c6279a4ab054f31908155bdce
José <jose@timekast.mx>
2026-06-30 07:47:05 -0600

    Install Google Tag Manager globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata, Viewport } from "next";
 import { Inter, Roboto_Slab } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { site } from "@/lib/site";
+
+const GTM_CONTAINER_ID = "GTM-N33N3MRM";
 
 const slab = Roboto_Slab({
   variable: "--font-roboto-slab",
@@ -94,7 +97,28 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       lang="en"
       className={`${slab.variable} ${inter.variable} h-full antialiased`}
     >
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_CONTAINER_ID}');`}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
       <body className="min-h-full flex flex-col bg-parchment text-hickory">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_CONTAINER_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
